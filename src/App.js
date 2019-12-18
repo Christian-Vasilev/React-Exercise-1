32dc6ae7079d99e28b13eaa95d3f28d51d67c9ee
Christian-Vasilev <KristianVasilevv@gmail.com>
2019-12-18 22:56:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+import logo from './logo.svg';
+/*
+Use React and the data below to display a list of users alongside their favorite movies.
+
+For detailed instructions, refer to instructions.md.
+*/
+
+
+
+class App extends Component {
+    render() {
+
+    }
+}
+
+export default App;
